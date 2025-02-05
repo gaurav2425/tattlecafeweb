6307bd5bbf6a9a8f7ae53b67063a4bb982e51356
iamgaurav18 <gauravburande2425@gmail.com>
2025-02-05 12:16:26 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistMono.className}`}>{children}</body>
+      <body className={`${geistSans.className}`}>{children}</body>
     </html>
   );
 }
