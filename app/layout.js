112e77cb703cda02def5fd5f8c3eb9c4073c63b9
iamgaurav18 <gauravburande2425@gmail.com>
2025-02-06 12:09:16 +0530

    Test

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.className}`}>{children}</body>
+      <body className={`${geistSans.geistMono}`}>{children}</body>
     </html>
   );
 }
