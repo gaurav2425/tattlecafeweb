b146a8f0c3b8c2bf867a99ea9aadb1dac859bafe
iamgaurav18 <gauravburande2425@gmail.com>
2025-03-06 12:18:59 +0530

    Increased size of menu bar

--- app/components/svg.js
+++ app/components/svg.js
@@ -94,9 +94,9 @@ const MenuIconSvg = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      viewBox="0 0 32 32"
+      width="32"
+      height="32"
       color="#ffffff"
       fill="none"
     >
